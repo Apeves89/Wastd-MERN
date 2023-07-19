8eb8d5c8e0dbea3567f9015b2118d0f9181f4bd4
Alexis Peves <alexpeves@gmail.com>
2023-07-19 02:30:18 -0400

    HomePage component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "../HomePage/HomePage";
 
 export default function App() {
   return (
-    <main className="App">
-      App
-    </main>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
-
